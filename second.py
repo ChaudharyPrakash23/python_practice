f5bcc5f3da95b74d7673573f99230833e629bdf6
prakash chaudhary <chaudharyprakash023pc@gmail.com>
2026-07-01 15:45:44 +0545

    first and second day

---
+++ second.py
@@ -0,0 +1,9 @@
+#comment is a part of coding file that the programmer donot want to execute , rather used to expalin or testing code
+#types of comment are single  line commment and multiline comment (ctrl+/)
+# this is single line commment
+print ("this is print statemant")
+# to enter characters that cannot be directly used in a string we use an escape sequence character
+# an escape sequence character is /(backslash) followed by character you want to insert.
+# print("thi cannot be "executed")
+print ("this can be executed")
+print("this","is" ,"seperator",sep="~")
